c3a50e6d321d3aac5b2e96d01c3a224b55515ad0
Crawlblade <k.kudsk@gmail.com>
2016-02-17 12:28:15 +0100

    ToiletPaper skal have en position, men ellers er den dun

---
+++ ToiletPaper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace MummyDispair
+{
+    class ToiletPaper : TypeComponent, ILoadable
+    {
+        private Animator animator;
+
+        public ToiletPaper(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            this.animator = (Animator)gameObject.GetComponent("Animator");
+
+            CreateAnimations();
+            animator.PlayAnimation("Standard");
+        }
+
+        private void CreateAnimations()
+        {
+            animator.CreateAnimation("Standard", new Animation(1, 0, 0, 35, 40, 0, Vector2.Zero, true));
+        }
+    }
+}
