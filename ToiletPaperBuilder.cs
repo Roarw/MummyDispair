3dc1d0badcc19fcb719ed62adfd88d5e78ccde8e
Crawlblade <k.kudsk@gmail.com>
2016-02-17 11:25:45 +0100

    dfhujk

---
+++ ToiletPaperBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MummyDispair
+{
+    class ToiletPaperBuilder 
+    {
+        private GameObject gameObject;
+
+        public ToiletPaperBuilder()
+        {
+
+        }
+
+        public void BuildGameObject(Vector2 position)
+        {
+            gameObject = new GameObject(position);
+
+        }
+    }
+}
