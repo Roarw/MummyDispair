b93d1973c3287103f0fee69c32891945e17d86a3
Roarw <zalwio@hotmail.dk>
2016-02-17 12:46:44 +0100

    set

--- ToiletPaperBuilder.cs
+++ ToiletPaperBuilder.cs
@@ -21,7 +21,7 @@ namespace MummyDispair
             gameObject.AddComponent(new SpriteRenderer(gameObject, "static/ToiletRoll.png", 0, Vector2.Zero));
             gameObject.AddComponent(new Animator(gameObject));
             gameObject.AddTypeComponent(new ToiletPaper(gameObject));
-            Collider collider = new Collider(gameObject);
+            Collider collider = new Collider(gameObject, true);
             collider.DoCollisionChecks = false;
             gameObject.AddComponent(collider);
         }
