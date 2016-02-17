007f6868241e29b351591a67fc28560ef7afac91
Roarw <zalwio@hotmail.dk>
2016-02-17 09:55:27 +0100

    Pyramid done

--- LevelCreator.cs
+++ LevelCreator.cs
@@ -23,27 +23,58 @@ namespace MummyDispair
 
         public GameObject AddToList()
         {
-            GameObject player = RunBuilder(new PlayerBuilder(), Vector2.Zero);
+            WallAt(0, 0);
+            WallRange(-1, 1, 1);
+            WallRange(-2, 2, 2);
+            WallRange(-3, -2, 3); WallRange(2, 3, 3);
+            WallRange(-4, -3, 4); WallRange(3, 4, 4);
+            WallRange(-5, -4, 5); WallRange(4, 5, 5);
+            WallRange(-6, -5, 6); WallRange(5, 6, 6);
+            WallRange(-7, -5, 7); WallRange(5, 7, 7);
+            WallRange(-8, -5, 8); /*hole*/ WallRange(-3, -1, 8); /*relic hole*/ WallRange(1, 8, 8);
+            WallRange(-9, -5, 9); WallRange(8, 9, 9);
+            WallRange(-10, -5, 10); WallRange(8, 10, 10);
+            WallRange(-11, -5, 11); WallRange(8, 11, 11);
+            WallRange(-12, -1, 12); /*relic hole*/ WallRange(1, 6, 12); /*hole*/ WallRange(8, 12, 12);
+            WallRange(-13, -12, 13); WallRange(8, 13, 13);
+            WallRange(-14, -12, 14); WallRange(8, 14, 14);
+            WallRange(-15, -12, 15); WallRange(8, 15, 15);
+            WallRange(-16, -12, 16); /*hole*/ WallRange(-10, -5, 16); /*almost relic hole*/ WallRange(-3, 16, 16);
+            WallRange(-17, -12, 17); WallRange(14, 17, 17);
+            WallRange(-18, -12, 18); WallRange(15, 18, 18);
+            WallRange(-19, -12, 19); WallRange(16, 19, 19);
+            WallRange(-20, 8, 20); /**/ WallRange(17, 20, 20);
+            WallRange(-21, -18, 21); WallAt(8, 21); /*spike*/ WallAt(10, 21); WallRange(17, 20, 21);
+            WallRange(-22, -19, 22); WallRange(8, 10, 22); /*spike*/ WallAt(12, 22); WallRange(17, 20, 22);
+            WallRange(-27, -20, 23); /*dart->*/WallAt(-10, 23); WallRange(9, 12, 23); /*spike*/ WallAt(14, 23); WallRange(17, 20, 23);
+            WallRange(-27, -26, 24); WallAt(-20, 24); /*dart->*/WallAt(-10, 24); WallRange(-9, 5, 24); WallRange(10, 14, 24); /*hole*/ WallRange(16, 20, 24);
+            WallRange(-27, -26, 25); WallRange(-16, -12, 25); WallRange(16, 20, 25);
+            WallRange(-27, -26, 26); WallRange(-17, -12, 26); WallRange(16, 20, 26);
+            WallRange(-27, -12, 27); WallRange(16, 20, 27);
+            WallRange(-27, 20, 28);
+            WallRange(-13, 17, 29);
 
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(0, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(100, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(200, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(300, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(400, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(500, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(600, 400));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(700, 400));
 
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(500, 300));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(600, 300));
-            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(700, 300));
+            GameObject player = RunBuilder(new PlayerBuilder(), new Vector2(-2400, 2500));
 
-            RunBuilder(new FemaleBuilder(), new Vector2(0, 257));
+            RunBuilder(new FemaleBuilder(), new Vector2(-2480, 2555));
 
-            RunBuilder(new ScorpionBuilder(), new Vector2(0, 20));
-
+            RunBuilder(new ScorpionBuilder(), new Vector2(-2300, 2400));
 
             return player;
+        }
+
+        private void WallRange(int x1, int x2, int y)
+        {
+            for (int x = x1; x <= x2; x++)
+            {
+                WallAt(x, y);
+            }
+        }
+
+        private void WallAt(int x, int y)
+        {
+            RunBuilder(new WallBuilder("WallBasic.png"), new Vector2(x * 100, y * 100));
         }
 
         private GameObject RunBuilder(IBuilder build, Vector2 position)
